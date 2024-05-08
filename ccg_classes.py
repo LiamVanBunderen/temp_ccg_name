8538a33cdeeb3b41763c06081a8f6ae5dd37ed9f
Liam VanBunderen <363436312@GOTVDSB.CA>
2024-05-08 09:41:53 -0400

    Started on Default Card Added the base of card generation, and started work on the missing texture textures.

--- ccg_classes.py
+++ ccg_classes.py
@@ -2,9 +2,12 @@ from dataclasses import dataclass
 from enum import Enum
 import pygame
 
+# cspell: ignore colorkey, blit
+
 
 class Family(Enum):
-    KNIGHT = 0
+    TEMP = 0
+    KNIGHT = 1
 
 
 @dataclass
@@ -21,4 +24,32 @@ class Card:
 
     def generate_art(self) -> pygame.Surface:
         card: pygame.Surface = pygame.Surface((250, 350))
-        key: pygame.Color
+        key: pygame.Color = pygame.Color(255, 100, 0)
+        base: pygame.Color = pygame.Color(255, 255, 245)
+
+        card.fill(key)
+        card.set_colorkey(key)
+
+        pygame.draw.rect(card, base, pygame.Rect(0, 0, 250, 350), 0, 15)
+
+        return card
+
+
+if __name__ == "__main__":
+
+    pygame.init()
+    screen = pygame.display.set_mode((350, 450))
+    pygame.display.set_caption("Testing_Card")
+
+    testCard = Card()
+
+    screen.blit(testCard.art, (50, 50))
+
+    pygame.display.flip()
+
+    while True:
+        for event in pygame.event.get():
+            match event.type:
+                case pygame.QUIT:
+                    pygame.quit()
+                    exit()
