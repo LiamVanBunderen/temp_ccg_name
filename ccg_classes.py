439a332453efa0f69c8e21865c5be7d21135eb69
Liam VanBunderen <363436312@GOTVDSB.CA>
2024-05-13 10:50:14 -0400

    Did Even More Stuff. I will write better commits later, I promise.

--- ccg_classes.py
+++ ccg_classes.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
+import json
 import pygame
 
 # cspell: ignore colorkey, blit
@@ -12,71 +13,77 @@ class Family(Enum):
 
 @dataclass
 class Card:
-    health: int = 20
-    damage: int = 5
-    energy: int = 1
-    movement: int = 1
-
     ID: str = "null"
-    NAME: str = "Null"
-    FAMILY: Family = Family.TEMP
 
     def __post_init__(self):
-        self.font: str = "taxon"
+        try:
+            json.load(open("cards.json"))
+
+        self.health: int = 20
+        self.damage: int = 5
+        self.energy: int = 1
+        self.movement: int = 1
+
+        self.NAME: str = "Null"
+        self.FAMILY: Family = Family.TEMP
+
         self.art: pygame.Surface = self.generate_art()
 
     def generate_art(self) -> pygame.Surface:
         card: pygame.Surface = pygame.Surface((250, 350))
-        try:
-            test: pygame.font.Font = pygame.font.Font(
-                f"assets/{self.FAMILY.value}/font/{self.font}.ttf"
-            )
-            base: pygame.Surface = pygame.image.load(f"assets/{self.FAMILY.value}/base.png")
-            character: pygame.Surface = pygame.image.load(
-                f"assets/{self.FAMILY.value}/{self.ID}/character_art.png"
-            )
-            desc: pygame.Surface = pygame.image.load(
-                f"assets/{self.FAMILY.value}/description_box.png"
-            )
-            empty: pygame.Surface = pygame.image.load(
-                f"assets/{self.FAMILY.value}/energy_empty.png"
-            )
-            full: pygame.Surface = pygame.image.load(
-                f"assets/{self.FAMILY.value}/energy_full.png"
-            )
-            moves: pygame.Surface = pygame.image.load(
-                f"assets/{self.FAMILY.value}/movement.png"
-            )
-            health_stat: pygame.Surface = pygame.image.load(
-                f"assets/{self.FAMILY.value}/health.png"
-            )
-            damage_stat: pygame.Surface = pygame.image.load(
-                f"assets/{self.FAMILY.value}/damage.png"
-            )
-            border: pygame.Surface = pygame.image.load(
-                f"assets/{self.FAMILY.value}/borders.png"
-            )
-        except:
-            
+        assets: dict[str, pygame.Surface] = {
+            "base": pygame.Surface((0, 0)),
+            "character_art": pygame.Surface((0, 0)),
+            "description_box": pygame.Surface((0, 0)),
+            "energy_empty": pygame.Surface((0, 0)),
+            "energy_full": pygame.Surface((0, 0)),
+            "movement": pygame.Surface((0, 0)),
+            "health": pygame.Surface((0, 0)),
+            "damage": pygame.Surface((0, 0)),
+            "borders": pygame.Surface((0, 0)),
+        }
+        font: pygame.font.Font
 
-        key: pygame.Color = base.get_at((0, 0))
+        for asset in assets:
+            extension: str = f"/{self.ID}" if asset == "character_art" else ""
+            try:
+                assets[asset] = pygame.image.load(
+                    f"assets/{self.FAMILY.value}/{asset}{extension}.png"
+                )
+            except FileNotFoundError:
+                extension = f"/null" if asset == "character_art" else ""
+                assets[asset] = pygame.image.load(f"assets/temp/{asset}{extension}.png")
+
+        try:
+            font = pygame.font.Font(f"assets/{self.FAMILY.value}/font.ttf")
+        except:
+            font = pygame.font.Font(f"assets/temp/font/taxon.ttf")
+
+        key: pygame.Color = assets["base"].get_at((0, 0))
 
         card.fill(key)
         card.set_colorkey(key)
 
-        card.blit(base, (0, 0))
-        card.blit(desc, (25, 245))
-        card.blit(character, (45, 35))
-        card.blit(health_stat, (41, 205))
-        card.blit(damage_stat, (126, 205))
+        card.blit(assets["base"], (0, 0))
+        card.blit(assets["description_box"], (25, 245))
+        card.blit(assets["character_art"], (45, 35))
+        card.blit(assets["health"], (41, 205))
+        card.blit(assets["damage"], (126, 205))
 
         for slot in range(10):
-            card.blit(full if (slot < self.energy) else empty, (15, (14 * slot) + 45))
+            card.blit(
+                (
+                    assets["energy_full"]
+                    if (slot < self.energy)
+                    else assets["energy_empty"]
+                ),
+                (15, (14 * slot) + 45),
+            )
 
         for slot in range(self.movement):
-            card.blit(moves, (215, (20 * slot) + 45))
+            card.blit(assets["movement"], (215, (20 * slot) + 45))
 
-        card.blit(border, (0, 0))
+        card.blit(assets["borders"], (0, 0))
         return card
 
 
@@ -99,6 +106,3 @@ if __name__ == "__main__":
                 case pygame.QUIT:
                     pygame.quit()
                     exit()
-
-def printError(error: str = "") -> None:
-    print(f"\u001b[31m{error}\u001b[0m")
