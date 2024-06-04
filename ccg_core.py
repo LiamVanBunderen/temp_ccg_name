d220b3ddc3d495db93d819cb2828107dc0216dca
Liam VanBunderen <363436312@GOTVDSB.CA>
2024-06-04 10:47:21 -0400

    Started Rendering Basic GUI Started rendering the base of the gui elements, and having them adapt to window resizing.

--- ccg_core.py
+++ ccg_core.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 
-# cspell: ignore Windowresized, blit, keydown, fullscreen
+# cspell: ignore Windowresized, blit, keydown, fullscreen, colorkey
 
 pygame.init()
 
@@ -19,10 +19,220 @@ clock = pygame.time.Clock()
 run = True
 
 BACKGROUND = pygame.Color(29, 117, 62)
+GUI_BASE = pygame.Color(25, 25, 25)
+HEALTH_BAR = pygame.Color(200, 50, 25)
+ENERGY_BAR = pygame.Color(10, 100, 255)
+COLOUR_KEY = pygame.Color(255, 0, 255)
 
 
 def drawBase():
+
+    base = pygame.Surface(window.get_window_size())
+    meat = pygame.Surface(window.get_window_size())
+    outline = pygame.Surface(window.get_window_size())
+
+    base.fill(COLOUR_KEY)
+    base.set_colorkey(COLOUR_KEY)
+    base.set_alpha(100)
+    pygame.draw.rect(
+        base,
+        GUI_BASE,
+        pygame.Rect(
+            5,
+            5,
+            window.get_window_size()[0] - 10,
+            max((window.get_window_size()[1] - 10) / 13, 5),
+        ),
+        0,
+    )
+    pygame.draw.rect(
+        base,
+        GUI_BASE,
+        pygame.Rect(
+            5,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 12,
+                (window.get_window_size()[1] - 10),
+            ),
+            (window.get_window_size()[0] - 10) / 7,
+            max((window.get_window_size()[1] - 10) / 26 - 2.5, 5),
+        ),
+        0,
+    )
+    pygame.draw.rect(
+        base,
+        GUI_BASE,
+        pygame.Rect(
+            5,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 12,
+                (window.get_window_size()[1] - 10),
+            )
+            + max((window.get_window_size()[1] - 10) / 26 + 2.5, 5),
+            (window.get_window_size()[0] - 10) / 7,
+            max((window.get_window_size()[1] - 10) / 26 - 2.5, 5),
+        ),
+        0,
+    )
+    pygame.draw.rect(
+        base,
+        GUI_BASE,
+        pygame.Rect(
+            (window.get_window_size()[0] - 10) / 7 * 6 + 5,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 12,
+                (window.get_window_size()[1] - 10),
+            ),
+            (window.get_window_size()[0] - 10) / 7,
+            max((window.get_window_size()[1] - 10) / 13, 5),
+        ),
+        0,
+    )
+    pygame.draw.rect(
+        base,
+        GUI_BASE,
+        pygame.Rect(
+            (window.get_window_size()[0] - 10) / 7 + 10,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 9,
+                (window.get_window_size()[1] - 10),
+            ),
+            (window.get_window_size()[0] - 10) / 7 * 5 - 10,
+            max((window.get_window_size()[1] - 10) / 13 * 4, 5),
+        ),
+        0,
+    )
+
+    meat.fill(COLOUR_KEY)
+    meat.set_colorkey(COLOUR_KEY)
+    pygame.draw.rect(
+        meat,
+        HEALTH_BAR,
+        pygame.Rect(
+            5,
+            5,
+            window.get_window_size()[0] - 10,
+            max((window.get_window_size()[1] - 10) / 13, 5),
+        ),
+        0,
+    )
+    pygame.draw.rect(
+        meat,
+        HEALTH_BAR,
+        pygame.Rect(
+            5,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 12,
+                (window.get_window_size()[1] - 10),
+            ),
+            (window.get_window_size()[0] - 10) / 7,
+            max((window.get_window_size()[1] - 10) / 26 - 2.5, 5),
+        ),
+        0,
+    )
+    pygame.draw.rect(
+        meat,
+        ENERGY_BAR,
+        pygame.Rect(
+            5,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 12,
+                (window.get_window_size()[1] - 10),
+            )
+            + max((window.get_window_size()[1] - 10) / 26 + 2.5, 5),
+            (window.get_window_size()[0] - 10) / 7,
+            max((window.get_window_size()[1] - 10) / 26 - 2.5, 5),
+        ),
+        0,
+    )
+
+    outlineWidth = max(
+        min(
+            5,
+            min(
+                (window.get_window_size()[0] - 10) // 26,
+                (window.get_window_size()[1] - 10),
+            )
+            // 2
+            - 2,
+        ),
+        1,
+    )
+    outline.fill(COLOUR_KEY)
+    outline.set_colorkey(COLOUR_KEY)
+    pygame.draw.rect(
+        outline,
+        GUI_BASE,
+        pygame.Rect(
+            5,
+            5,
+            window.get_window_size()[0] - 10,
+            max((window.get_window_size()[1] - 10) / 13, 5),
+        ),
+        outlineWidth,
+    )
+    pygame.draw.rect(
+        outline,
+        GUI_BASE,
+        pygame.Rect(
+            5,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 12,
+                (window.get_window_size()[1] - 10),
+            ),
+            (window.get_window_size()[0] - 10) / 7,
+            max((window.get_window_size()[1] - 10) / 26 - 2.5, 5),
+        ),
+        max(min(outlineWidth * 2, 10) // 2, 1),
+    )
+    pygame.draw.rect(
+        outline,
+        GUI_BASE,
+        pygame.Rect(
+            5,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 12,
+                (window.get_window_size()[1] - 10),
+            )
+            + max((window.get_window_size()[1] - 10) / 26 + 2.5, 5),
+            (window.get_window_size()[0] - 10) / 7,
+            max((window.get_window_size()[1] - 10) / 26 - 2.5, 5),
+        ),
+        max(min(outlineWidth * 2, 10) // 2, 1),
+    )
+    pygame.draw.rect(
+        outline,
+        GUI_BASE,
+        pygame.Rect(
+            (window.get_window_size()[0] - 10) / 7 * 6 + 5,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 12,
+                (window.get_window_size()[1] - 10),
+            ),
+            (window.get_window_size()[0] - 10) / 7,
+            max((window.get_window_size()[1] - 10) / 13, 5),
+        ),
+        outlineWidth,
+    )
+    pygame.draw.rect(
+        outline,
+        GUI_BASE,
+        pygame.Rect(
+            (window.get_window_size()[0] - 10) / 7 + 10,
+            min(
+                (window.get_window_size()[1] - 5) / 13 * 9,
+                (window.get_window_size()[1] - 10),
+            ),
+            (window.get_window_size()[0] - 10) / 7 * 5 - 10,
+            max((window.get_window_size()[1] - 10) / 13 * 4, 5),
+        ),
+        outlineWidth,
+    )
+
     screen.fill(BACKGROUND)
+    screen.blit(base, (0, 0))
+    screen.blit(meat, (0, 0))
+    screen.blit(outline, (0, 0))
 
 
 drawBase()
@@ -37,8 +247,6 @@ while run:
                 screen = window.set_mode(window.get_window_size(), pygame.RESIZABLE)
             case pygame.KEYDOWN:
                 match (event.key):
-                    case pygame.K_F11:
-                        window.toggle_fullscreen()
                     case _:
                         pass
 
